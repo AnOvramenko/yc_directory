e6929522390ad61620b1da25ee2a96f0ac4ae08f
Ovramenko Andrii <anovramenko2@gmail.com>
2025-05-27 22:25:41 +0300

    fix user profile link

--- app/(root)/user/[id]/page.tsx
+++ app/(root)/user/[id]/page.tsx
@@ -44,7 +44,7 @@ const UserProfile = async ({ params }: UserProfileProps) => {
 
         <div className="flex-1 flex flex-col gap-5 lg:-mt-5">
           <p className="text-30-bold">
-            {session.id === id ? "Your" : "All"} Startups
+            {session?.id === id ? "Your" : "All"} Startups
           </p>
           <ul className="card_grid-sm">
             <Suspense fallback={<StartupCardSkeleton />}>
